d745abdc4639585921baf3269eab46c426e060ac
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-03-04 18:23:32 +0100

    update index videgame

--- resources/views/videogames/index.blade.php
+++ resources/views/videogames/index.blade.php
@@ -3,8 +3,45 @@
 @section('page-title', 'Home')
 
 @section('main-content')
-<h1>
-    Welcome
+<h1 class="text-center">
+    Videogames
 </h1>
+
+
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">TITOLO</th>
+            <th scope="col">PREZZO</th>
+            <th scope="col">TIPO</th>
+            <th scope="col">AZIONI</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">1</th>
+            @foreach ($videgames as $videogame)
+                <td>
+                    {{ $videogame->id }}
+                </td>
+                <td>
+                    {{ $videogame->title }}
+                </td>
+                <td>
+                    {{ $videogame->price }}
+                </td>
+                <td>
+                    {{ $videogame->type }}
+                </td>
+                <td>
+                    <a href="#" class="btn btn-primary">
+                        Vedi
+                    </a>
+                </td>
+            @endforeach
+        </tr>
+    </tbody>
+</table>
 
 @endsection
