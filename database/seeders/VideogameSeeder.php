68a8c0ec9fb7bb9a8c2c86df90f06d36650df2d5
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-03-04 17:04:50 +0100

    - update seeder ( Jordan )

--- database/seeders/VideogameSeeder.php
+++ database/seeders/VideogameSeeder.php
@@ -14,14 +14,16 @@ class VideogameSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
+    {   
+        videogame::truncate();
+        
         for ($i = 0; $i < 5; $i++) {
             $videogame = new videogame();
 
             $videogame->title = fake()->sentence;
             $videogame->description = fake()->paragraph;
             $videogame->thumb = fake()->imageUrl;
-            $videogame->price = fake()->randomFloat;
+            $videogame->price = fake()->randomFloat(6,2);
             $videogame->save();
         }
     }
